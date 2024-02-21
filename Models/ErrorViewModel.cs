180cbd78b50d825e604f4cd85124a30f3adf18f7
Jaime Futter <ST10067405@vcconnect.edu.za>
2024-02-21 08:58:42 +0200

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EpicBookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
